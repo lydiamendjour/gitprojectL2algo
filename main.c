5d0eb95affaf696b094187b9c2442d930ef036e5
lydiamendjour <155400932+lydiamendjour@users.noreply.github.com>
2024-01-12 20:22:34 +0100

    création de la liste doubelemnt chainée

--- main.c
+++ main.c
@@ -8,3 +8,25 @@ typedef struct Node {
     struct Node* prev;
     struct Node* next;
 } Node;
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertNode(int data) {
+    Node* newNode = createNode(data);
+
+    if (head == NULL) {
+        head = newNode;
+    } else {
+        Node* current = head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+        newNode->prev = current;
+    }
+}
